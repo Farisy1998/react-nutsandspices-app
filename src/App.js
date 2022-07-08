1c58c2d10dcd2c959bc8dfa2919b349e0e5c588e
Farisy1998 <salmanulfarisyk123@gmail.com>
2022-07-08 14:39:28 +0530

    farisy's commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/navbar";
+import Nav from "./components/home/nav";
+import Carousel from "./components/home/carousel/carousel";
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar />
+      <Nav />
+      <Carousel />
     </React.Fragment>
   );
 }
--- src/components/home/carousel/carousel.jsx
+++ src/components/home/carousel/carousel.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import "./home.css";
+import Card from "../card/card";
+import './carousel.css';
 
-class Home extends Component {
+class Carousel extends Component {
   state = {
-    imgsCarouselUrls: [
+    carouselImgs: [
       "https://images.pexels.com/photos/7956510/pexels-photo-7956510.jpeg?auto=compress&cs=tinysrgb&w=2300&h=1900&dpr=1",
-      "https://images.pexels.com/photos/5505092/pexels-photo-5505092.jpeg?auto=compress&cs=tinysrgb&w=2300&h=1900&dpr=1",
-      "https://images.pexels.com/photos/7412065/pexels-photo-7412065.jpeg?auto=compress&cs=tinysrgb&w=2300&h=1900&dpr=1",
+      "https://images.pexels.com/photos/3997459/pexels-photo-3997459.jpeg?auto=compress&cs=tinysrgb&w=2300&h=1900&dpr=1",
+      "https://images.pexels.com/photos/4085266/pexels-photo-4085266.jpeg?auto=compress&cs=tinysrgb&w=2300&h=1900&dpr=1",
     ],
   };
   render() {
-    const { imgsCarouselUrls } = this.state;
+    const { carouselImgs } = this.state;
     return (
-      <React.Fragment>
+      <Card className="carousel">
         <div
           id="carouselExampleIndicators"
           className="carousel slide"
@@ -31,53 +32,29 @@ class Home extends Component {
             <div className="carousel-item active">
               <img
                 className="d-block w-100"
-                src={imgsCarouselUrls[0]}
+                src={carouselImgs[0]}
                 alt="First slide"
               />
             </div>
             <div className="carousel-item">
               <img
                 className="d-block w-100"
-                src={imgsCarouselUrls[1]}
+                src={carouselImgs[1]}
                 alt="Second slide"
               />
             </div>
             <div className="carousel-item">
               <img
                 className="d-block w-100"
-                src={imgsCarouselUrls[2]}
+                src={carouselImgs[2]}
                 alt="Third slide"
               />
             </div>
           </div>
-          <a
-            className="carousel-control-prev"
-            href="#carouselExampleIndicators"
-            role="button"
-            data-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a
-            className="carousel-control-next"
-            href="#carouselExampleIndicators"
-            role="button"
-            data-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="sr-only">Next</span>
-          </a>
         </div>
-      </React.Fragment>
+      </Card>
     );
   }
 }
 
-export default Home;
+export default Carousel;
